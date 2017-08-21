2c6bc133abae9aa021367e18c791df7230372693
lucke71 <lucas.assis@inep.gov.br>
2017-08-21 19:56:21 -0300

    Adicionando tabelas para dissertacao

--- Tabelas unb e df/count de linhas e colunas.sql
+++ Tabelas unb e df/count de linhas e colunas.sql
@@ -1,3 +1,29 @@
+--Tabela 1 - UnB Total empilhada
+select	count(1) as num_vinculos,
+		co_aluno_situacao,
+		case when co_ocde_area_geral is null then 0 else co_ocde_area_geral end as ocde
+from unb_empilhada
+group by co_aluno_situacao,case when co_ocde_area_geral is null then 0 else co_ocde_area_geral end;
+
+
+--Tabela 2 - UnB graduação presencial
+select	count(1) as num_vinculos,
+		co_aluno_situacao,
+		case when co_ocde_area_geral is null then 0 else co_ocde_area_geral end as ocde
+from unb_empilhada
+where co_nivel_academico=1 and co_modalidade_ensino=1
+group by co_aluno_situacao,case when co_ocde_area_geral is null then 0 else co_ocde_area_geral end;
+
+
+--Tabela 3 - UnB ingressantes e graduação presencial
+select	count(1) as num_vinculos,
+		co_aluno_situacao,
+		case when co_ocde_area_geral is null then 0 else co_ocde_area_geral end as ocde
+from unb_ingress_teste
+group by co_aluno_situacao,case when co_ocde_area_geral is null then 0 else co_ocde_area_geral end;
+
+
+
 select	count(1)
 from unb_curso_ingressantes; 
 
