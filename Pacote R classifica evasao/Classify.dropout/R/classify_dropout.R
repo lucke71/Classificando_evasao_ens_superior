97f56ebcfd64f737d027d162ed6b4cf227914b67
lucke71 <lucas.assis@inep.gov.br>
2017-11-14 14:50:09 -0200

    modificando padrao e saida como lista

--- Pacote R classifica evasao/Classify.dropout/R/classify_dropout.R
+++ Pacote R classifica evasao/Classify.dropout/R/classify_dropout.R
@@ -1,5 +1,5 @@
 classify_dropout <-
-  function(database,cv_folds=4,pct_training=0.75,classifier="CART",num_cores=4,balance="no",preprocess="yes"){
+  function(database,cv_folds=4,pct_training=0.75,classifier="CART",num_cores=2,balance="down",preprocess="yes"){
     library(dplyr)
     library(caret)
     
@@ -149,17 +149,20 @@ classify_dropout <-
     # Classificando com Naive Bayes
     if(classifier=="all" | classifier=="NB"){
       if(balance=="up"){
-        naive_fit_up <<- train(x = dplyr::select(up_data,-evasao),y=up_data$evasao,method="nb",trControl=fitcontrol,metric = 'Spec')
-        naive_pred_up <<- predict(naive_fit_up,base_teste)
-        mc_naive_up <<- confusionMatrix(naive_pred_up,base_teste$evasao)
+        naive_fit_up = train(x = dplyr::select(up_data,-evasao),y=up_data$evasao,method="nb",trControl=fitcontrol,metric = 'Spec')
+        naive_pred_up = predict(naive_fit_up,base_teste)
+        mc_naive_up = confusionMatrix(naive_pred_up,base_teste$evasao)
+        list(naive_fit_up,naive_pred_up,mc_naive_up)
       }else if(balance=="down"){
-        naive_fit_dwn <<- train(x = dplyr::select(dwn_data,-evasao),y=dwn_data$evasao,method="nb",trControl=fitcontrol,metric = 'Spec')
-        naive_pred_dwn <<- predict(naive_fit_dwn,base_teste)
-        mc_naive_dwn <<- confusionMatrix(naive_pred_dwn,base_teste$evasao)
+        naive_fit_dwn = train(x = dplyr::select(dwn_data,-evasao),y=dwn_data$evasao,method="nb",trControl=fitcontrol,metric = 'Spec')
+        naive_pred_dwn = predict(naive_fit_dwn,base_teste)
+        mc_naive_dwn = confusionMatrix(naive_pred_dwn,base_teste$evasao)
+        list(naive_fit_dwn,naive_pred_dwn,mc_naive_dwn)
       }else{
-        naive_fit <<- train(x = dplyr::select(base_treina,-evasao),y=base_treina$evasao,method="nb",trControl=fitcontrol,metric = 'Spec')
-        naive_pred <<- predict(naive_fit,base_teste)
-        mc_naive <<- confusionMatrix(naive_pred,base_teste$evasao)
+        naive_fit = train(x = dplyr::select(base_treina,-evasao),y=base_treina$evasao,method="nb",trControl=fitcontrol,metric = 'Spec')
+        naive_pred = predict(naive_fit,base_teste)
+        mc_naive = confusionMatrix(naive_pred,base_teste$evasao)
+        list(naive_fit,naive_pred,mc_naive)
       }
     }
     
@@ -167,17 +170,20 @@ classify_dropout <-
     # Classificando com CART
     if(classifier=="all" | classifier=="CART"){
       if(balance=="up"){
-        cart_fit_up <<- train(x = dplyr::select(up_data,-evasao),y=up_data$evasao,method="rpart",trControl=fitcontrol,tuneLength = 10,metric = 'Spec')
-        cart_pred_up <<- predict(cart_fit_up,base_teste)
-        mc_cart_up <<- confusionMatrix(cart_pred_up,base_teste$evasao)
+        cart_fit_up = train(x = dplyr::select(up_data,-evasao),y=up_data$evasao,method="rpart",trControl=fitcontrol,tuneLength = 10,metric = 'Spec')
+        cart_pred_up = predict(cart_fit_up,base_teste)
+        mc_cart_up = confusionMatrix(cart_pred_up,base_teste$evasao)
+        list(cart_fit_up,cart_pred_up,mc_cart_up)
       }else if(balance=="down"){
-        cart_fit_dwn <<- train(x = dplyr::select(dwn_data,-evasao),y=dwn_data$evasao,method="rpart",trControl=fitcontrol,tuneLength = 10,metric = 'Spec')
-        cart_pred_dwn <<- predict(cart_fit_dwn,base_teste)
-        mc_cart_dwn <<- confusionMatrix(cart_pred_dwn,base_teste$evasao)
+        cart_fit_dwn = train(x = dplyr::select(dwn_data,-evasao),y=dwn_data$evasao,method="rpart",trControl=fitcontrol,tuneLength = 10,metric = 'Spec')
+        cart_pred_dwn = predict(cart_fit_dwn,base_teste)
+        mc_cart_dwn = confusionMatrix(cart_pred_dwn,base_teste$evasao)
+        list(cart_fit_dwn,cart_pred_dwn,mc_cart_dwn)
       }else{
-        cart_fit <<- train(x = dplyr::select(base_treina,-evasao),y=base_treina$evasao,method="rpart",trControl=fitcontrol,tuneLength = 10,metric = 'Spec')
-        cart_pred <<- predict(cart_fit,base_teste)
-        mc_cart <<- confusionMatrix(cart_pred,base_teste$evasao)
+        cart_fit = train(x = dplyr::select(base_treina,-evasao),y=base_treina$evasao,method="rpart",trControl=fitcontrol,tuneLength = 10,metric = 'Spec')
+        cart_pred = predict(cart_fit,base_teste)
+        mc_cart = confusionMatrix(cart_pred,base_teste$evasao)
+        list(cart_fit,cart_pred,mc_cart)
       }
     }
     
@@ -186,17 +192,20 @@ classify_dropout <-
     if(classifier=="all" | classifier=="C50"){
       grid <- expand.grid( .winnow = c(TRUE,FALSE), .trials=c(1,10,20,30,40), .model="tree" )
       if(balance=="up"){
-        c50_fit_up <<- train(x=dplyr::select(up_data,-evasao),y=up_data$evasao,method="C5.0",tuneGrid = grid,trControl=fitcontrol,metric = 'Spec') 
-        c50_pred_up <<- predict(c50_fit_up,base_teste)
-        mc_c50_up <<- confusionMatrix(c50_pred_up,base_teste$evasao)
+        c50_fit_up = train(x=dplyr::select(up_data,-evasao),y=up_data$evasao,method="C5.0",tuneGrid = grid,trControl=fitcontrol,metric = 'Spec') 
+        c50_pred_up = predict(c50_fit_up,base_teste)
+        mc_c50_up = confusionMatrix(c50_pred_up,base_teste$evasao)
+        list(c50_fit_up,c50_pred_up,mc_c50_up)
       }else if(balance=="down"){
-        c50_fit_dwn <<- train(x=dplyr::select(dwn_data,-evasao),y=dwn_data$evasao,method="C5.0",tuneGrid = grid,trControl=fitcontrol,metric = 'Spec') 
-        c50_pred_dwn <<- predict(c50_fit_dwn,base_teste)
-        mc_c50_dwn <<- confusionMatrix(c50_pred_dwn,base_teste$evasao)
+        c50_fit_dwn = train(x=dplyr::select(dwn_data,-evasao),y=dwn_data$evasao,method="C5.0",tuneGrid = grid,trControl=fitcontrol,metric = 'Spec') 
+        c50_pred_dwn = predict(c50_fit_dwn,base_teste)
+        mc_c50_dwn = confusionMatrix(c50_pred_dwn,base_teste$evasao)
+        list(c50_fit_dwn,c50_pred_dwn,mc_c50_dwn)
       }else{
-        c50_fit <<- train(x=dplyr::select(base_treina,-evasao),y=base_treina$evasao,method="C5.0",tuneGrid = grid,trControl=fitcontrol,metric = 'Spec') 
-        c50_pred <<- predict(c50_fit,base_teste)
-        mc_c50 <<- confusionMatrix(c50_pred,base_teste$evasao)
+        c50_fit = train(x=dplyr::select(base_treina,-evasao),y=base_treina$evasao,method="C5.0",tuneGrid = grid,trControl=fitcontrol,metric = 'Spec') 
+        c50_pred = predict(c50_fit,base_teste)
+        mc_c50 = confusionMatrix(c50_pred,base_teste$evasao)
+        list(c50_fit,c50_pred,mc_c50)
       }
     }
     
@@ -204,34 +213,40 @@ classify_dropout <-
     # Classificando com regressao logistica
     if(classifier=="all" | classifier=="reglog"){
       if(balance=="up"){
-        reglog_fit_up <<- train(evasao ~ .,data=up_data ,method="glm",family=binomial(link="logit"),trControl=fitcontrol,metric = 'Spec',maxit=100) 
-        reg_pred_up <<- predict(reglog_fit_up,base_teste)
-        mc_reg_up <<- confusionMatrix(reg_pred_up,base_teste$evasao)
+        reglog_fit_up = train(evasao ~ .,data=up_data ,method="glm",family=binomial(link="logit"),trControl=fitcontrol,metric = 'Spec',maxit=100) 
+        reg_pred_up = predict(reglog_fit_up,base_teste)
+        mc_reg_up = confusionMatrix(reg_pred_up,base_teste$evasao)
+        list(reglog_fit_up,reg_pred_up,mc_reg_up)
       }else if(balance=="down"){
-        reglog_fit_dwn <<- train(evasao ~ .,data=dwn_data ,method="glm",family=binomial(link="logit"),trControl=fitcontrol,metric = 'Spec',maxit=100) 
-        reg_pred_dwn <<- predict(reglog_fit_dwn,base_teste)
-        mc_reg_dwn <<- confusionMatrix(reg_pred_dwn,base_teste$evasao)
+        reglog_fit_dwn = train(evasao ~ .,data=dwn_data ,method="glm",family=binomial(link="logit"),trControl=fitcontrol,metric = 'Spec',maxit=100) 
+        reg_pred_dwn = predict(reglog_fit_dwn,base_teste)
+        mc_reg_dwn = confusionMatrix(reg_pred_dwn,base_teste$evasao)
+        list(reglog_fit_dwn,reg_pred_dwn,mc_reg_dwn)
       }else{
-        reglog_fit <<- train(evasao ~ .,data=base_treina ,method="glm",family=binomial(link="logit"),trControl=fitcontrol,metric = 'Spec',maxit=100) 
-        reg_pred <<- predict(reglog_fit,base_teste)
-        mc_reg <<- confusionMatrix(reg_pred,base_teste$evasao)
+        reglog_fit = train(evasao ~ .,data=base_treina ,method="glm",family=binomial(link="logit"),trControl=fitcontrol,metric = 'Spec',maxit=100) 
+        reg_pred = predict(reglog_fit,base_teste)
+        mc_reg = confusionMatrix(reg_pred,base_teste$evasao)
+        list(reglog_fit,reg_pred,mc_reg)
       }
     }
     
     # Classificando com redes neurais
     if(classifier=="all" | classifier=="Nnet"){
       if(balance=="up"){
-        nnet_fit_up <<- train(evasao ~ .,data=up_data,method="nnet",trControl=fitcontrol,metric = 'Spec', maxit=1000,MaxNWts=3000)
-        nnet_pred_up <<- predict(nnet_fit_up,base_teste)
-        mc_nnet_up <<- confusionMatrix(nnet_pred_up,base_teste$evasao)
+        nnet_fit_up = train(evasao ~ .,data=up_data,method="nnet",trControl=fitcontrol,metric = 'Spec', maxit=1000,MaxNWts=3000)
+        nnet_pred_up = predict(nnet_fit_up,base_teste)
+        mc_nnet_up = confusionMatrix(nnet_pred_up,base_teste$evasao)
+        list(nnet_fit_up,nnet_pred_up,mc_nnet_up)
       }else if(balance=="down"){
-        nnet_fit_dwn <<- train(evasao ~ .,data=dwn_data,method="nnet",trControl=fitcontrol,metric = 'Spec', maxit=1000,MaxNWts=3000) 
-        nnet_pred_dwn <<- predict(nnet_fit_dwn,base_teste)
-        mc_nnet_dwn <<- confusionMatrix(nnet_pred_dwn,base_teste$evasao)
+        nnet_fit_dwn = train(evasao ~ .,data=dwn_data,method="nnet",trControl=fitcontrol,metric = 'Spec', maxit=1000,MaxNWts=3000) 
+        nnet_pred_dwn = predict(nnet_fit_dwn,base_teste)
+        mc_nnet_dwn = confusionMatrix(nnet_pred_dwn,base_teste$evasao)
+        list(nnet_fit_dwn,nnet_pred_dwn,mc_nnet_dwn)
       }else{
-        nnet_fit <<- train(evasao ~ ., data=base_treina,method="nnet",trControl=fitcontrol,metric = 'Spec', maxit=1000,MaxNWts=3000) 
-        nnet_pred <<- predict(nnet_fit,base_teste)
-        mc_nnet <<- confusionMatrix(nnet_pred,base_teste$evasao)
+        nnet_fit = train(evasao ~ ., data=base_treina,method="nnet",trControl=fitcontrol,metric = 'Spec', maxit=1000,MaxNWts=3000) 
+        nnet_pred = predict(nnet_fit,base_teste)
+        mc_nnet = confusionMatrix(nnet_pred,base_teste$evasao)
+        list(nnet_fit,nnet_pred,mc_nnet)
       }
     }
   }
